9404bbb276af56b1086e6eef7034d8a7047c2a6d
flores8 <flores.lauralee@gmail.com>
2014-09-03 14:30:47 -0700

    updated some javascript events and the home page

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -14,14 +14,14 @@ var sendTrackableEvent = function(web_property_id, type){
 	// _bm_request.send(JSON.stringify(_bm_event));
 }
 
-//jquery event handlers (page views (not sure... maybe .ready(), click (.click), on hover(.hover), )
+
 $(document).ready(function() { 
 	var ctx = $("#myChart").get(0).getContext("2d");
 	var myNewChart = new Chart(ctx);
 
 	// Line graph
 	var data = {
-		labels : JSON.parse($("#value-label").html()),
+		labels : JSON.parse({$("#value-label").html()),
 		datasets : [
 			{
 				fillColor : "rgba(151,187,205,0.5)",
