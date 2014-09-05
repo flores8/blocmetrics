31d85e7a8cb96ddd437fca0106eb8cd39ffbca26
flores8 <flores.lauralee@gmail.com>
2014-09-05 11:40:43 -0700

    added line graph js

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -12,3 +12,24 @@ var sendTrackableEvent = function(web_property_id, type){
 
 	// _bm_request.send(JSON.stringify(_bm_event));
 }
+
+
+$(document).ready(function() { 
+	var ctx = $("#myChart").get(0).getContext("2d");
+	var myNewChart = new Chart(ctx);
+
+	// Line graph
+	var data = {
+		labels : JSON.parse({$("#value-label").html()),
+		datasets : [
+			{
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,1)",
+				pointColor : "rgba(151,187,205,1)",
+				pointStrokeColor : "#fff",
+				data : JSON.parse($("#value-data").html())
+			}
+		]
+	}
+	new Chart(ctx).Line(data);
+ });
